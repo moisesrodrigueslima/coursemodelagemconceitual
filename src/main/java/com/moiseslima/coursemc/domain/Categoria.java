17d89891da109eb7585a26229fb584a8a71c541d
Moisés Lima <moises.lima2000@gmail.com>
2020-08-15 17:41:12 -0300

    Banco de dados H2 e criação auto da base de dados

--- src/main/java/com/moiseslima/coursemc/domain/Categoria.java
+++ src/main/java/com/moiseslima/coursemc/domain/Categoria.java
@@ -2,10 +2,18 @@ package com.moiseslima.coursemc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L; //Exigencia da classe java.
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
