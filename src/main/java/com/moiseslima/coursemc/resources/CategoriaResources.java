a258c5b927f8974e4e8326511188c8ebf34d33c9
Moisés Lima <moises.lima2000@gmail.com>
2020-08-21 16:55:09 -0300

    Atualizando categoria com PUT, outros pequenos ajustes

--- src/main/java/com/moiseslima/coursemc/resources/CategoriaResources.java
+++ src/main/java/com/moiseslima/coursemc/resources/CategoriaResources.java
@@ -22,8 +22,8 @@ public class CategoriaResources {
 	private CategoriaService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok(obj);
 	}
 
@@ -38,5 +38,14 @@ public class CategoriaResources {
 		
 	}
 	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update (@RequestBody Categoria obj, @PathVariable Integer id)  {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+		
+		
+		
+	}
 	
 }
--- src/main/java/com/moiseslima/coursemc/services/CategoriaService.java
+++ src/main/java/com/moiseslima/coursemc/services/CategoriaService.java
@@ -17,7 +17,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	 
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 	
 		Optional<Categoria> obj = repo.findById(id);
 		
@@ -32,6 +32,12 @@ public class CategoriaService {
 		
 	}
 
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	
+	}
+	
 	
 	
 			
