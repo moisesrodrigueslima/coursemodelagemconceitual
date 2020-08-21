abd71e9bf566266ffeab0e83597bcbdb34ea90ac
Moisés Lima <moises.lima2000@gmail.com>
2020-08-21 11:53:44 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/moiseslima/coursemc/CoursemcApplication.java
+++ src/main/java/com/moiseslima/coursemc/CoursemcApplication.java
@@ -1,5 +1,6 @@
 package com.moiseslima.coursemc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.moiseslima.coursemc.domain.Cidade;
 import com.moiseslima.coursemc.domain.Cliente;
 import com.moiseslima.coursemc.domain.Endereco;
 import com.moiseslima.coursemc.domain.Estado;
+import com.moiseslima.coursemc.domain.Pagamento;
+import com.moiseslima.coursemc.domain.PagamentoComBoleto;
+import com.moiseslima.coursemc.domain.PagamentoComCartao;
+import com.moiseslima.coursemc.domain.Pedido;
 import com.moiseslima.coursemc.domain.Produto;
+import com.moiseslima.coursemc.domain.enums.EstadoPagamento;
 import com.moiseslima.coursemc.domain.enums.TipoCliente;
 import com.moiseslima.coursemc.repositories.CategoriaRepository;
 import com.moiseslima.coursemc.repositories.CidadeRepository;
 import com.moiseslima.coursemc.repositories.ClienteRepository;
 import com.moiseslima.coursemc.repositories.EnderecoRepository;
 import com.moiseslima.coursemc.repositories.EstadoRepository;
+import com.moiseslima.coursemc.repositories.PagamentoRepository;
+import com.moiseslima.coursemc.repositories.PedidoRepository;
 import com.moiseslima.coursemc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -42,7 +50,11 @@ public class CoursemcApplication implements CommandLineRunner {
 	@Autowired
 	private EnderecoRepository enderecoRepository;
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	
 	
@@ -99,6 +111,30 @@ public class CoursemcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
+		// Mascara de formatação para instanciar uma Data.
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+	
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		
+		
+		
+		
 		
 		
 		
---
+++ src/main/java/com/moiseslima/coursemc/repositories/PagamentoRepository.java
@@ -0,0 +1,14 @@
+package com.moiseslima.coursemc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.moiseslima.coursemc.domain.Pagamento;
+
+@Repository
+public interface  PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+	
+	
+	
+}
